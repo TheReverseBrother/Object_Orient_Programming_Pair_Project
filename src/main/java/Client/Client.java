c3fed475d2099d3b0cf3b41a6eacf63308c4b1a4
Unknown <d00212721@student.dkit.ie>
2019-04-10 21:25:11 +0100

    added user register

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -1,4 +1,5 @@
 package Client;
+import DAOs.MysqlUserDAO;
 
 public class Client
 {
@@ -8,10 +9,19 @@ public class Client
     public static  void setConnectionToFalse(){connection =  false;}
     public static  ClientServerConnection ClientServer = new ClientServerConnection();
     public static int USERID = -1;
+    public  static MysqlUserDAO userDAO = new MysqlUserDAO();
 
     public static void main(String[] args)
     {
         Menus.mainMenu();
     }
 
+    public static void editMovie(int user_ID) {
+    }
+
+    public static void getWatchedMovies(int user_ID) {
+    }
+
+    public static void addMovieToWatched(int user_ID) {
+    }
 }
