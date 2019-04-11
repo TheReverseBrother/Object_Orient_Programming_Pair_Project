2f0ddc81b3926bd73a01c26520f8d6ae4709f112
Aaron <d00212721@student.dkit.ie>
2019-04-11 17:45:23 +0100

    Major Error fix

--- src/main/java/DAOs/WatchedDAO.java
+++ src/main/java/DAOs/WatchedDAO.java
@@ -19,6 +19,172 @@ public class WatchedDAO extends MysqlDAO implements WatchedDAOInterface
         int userID = Integer.parseInt(user);
         Connection con = null;
         PreparedStatement ps = null;
+        ResultSet rs = null;
+        JSONArray WatchedList = new JSONArray();
+        try
+        {
+            con = this.getConnection();
+            String query = "SELECT * FROM WATCHEDTABLE WHERE USERID=?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1,userID);
+
+            rs = ps.executeQuery();
+            if(rs.next())
+            {
+                rs.beforeFirst();
+                while(rs.next())
+                {
+                    String title   = rs.getString("MovieTitle");
+                    String movieID = rs.getString("movieID");
+
+                    JSONObject Watched = new JSONObject();
+                    Watched.put("title      ",""+title);
+                    Watched.put("movieID      ",""+movieID);
+
+                    WatchedList.put(Watched);
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        catch(SQLException e)
+        {
+
+        }
+        finally {
+            try{
+                if(rs !=null)
+                {
+                    rs.close();
+                }
+                if(ps != null)
+                {
+                    ps.close();
+                }
+                if( con != null)
+                {
+                    this.freeConnection(con);
+                }
+            }
+            catch(SQLException e)
+            {
+            }
+        }
+
+        return WatchedList;
+    }
+
+
+    @Override
+    public String AddtoWatched(String user, String movie, String title)
+    {
+        int userID = Integer.parseInt(user);
+        int movieID = Integer.parseInt(movie);
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String result = "";
+        try
+        {
+            System.out.println("Fight Me");
+            con = this.getConnection();
+            String query = "INSERT INTO WATCHEDTABLE(USERID,MOVIEID,MOVIETITLE) VALUES(?,?,?)";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, userID);
+            ps.setInt(2, movieID);
+            ps.setString(3, title);
+            ps.executeUpdate();
+            result = "SuccesfullyAdded";
+
+        }
+        catch (SQLException e)
+        {
+            System.out.println("SQL");
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (rs != null)
+                {
+                    rs.close();
+                }
+                if (ps != null)
+                {
+                    ps.close();
+                }
+                if (con != null)
+                {
+                    this.freeConnection(con);
+                }
+            }
+            catch (SQLException e)
+            {
+
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Boolean CheckIfWatched(String user, String movie)
+    {
+        int userID = Integer.parseInt(user);
+        int movieID = Integer.parseInt(movie);
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try
+        {
+            con = this.getConnection();
+            String query = "SELECT * FROM WATCHEDTABLE WHERE USERID=? AND MOVIEID=?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, userID);
+            ps.setInt(2, movieID);
+            rs = ps.executeQuery();
+
+            return rs.next();
+        }
+        catch (SQLException e)
+        {
+
+        }
+        finally
+        {
+            try
+            {
+                if (rs != null)
+                {
+                    rs.close();
+                }
+                if (ps != null)
+                {
+                    ps.close();
+                }
+                if (con != null)
+                {
+                    this.freeConnection(con);
+                }
+            }
+            catch (SQLException e)
+            {
+            }
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public JSONArray GetAllWatchedMoviesForClient(String user)
+    {
+        int userID = Integer.parseInt(user);
+        Connection con = null;
+        PreparedStatement ps = null;
         JSONArray emptyArray = new JSONArray();
         ResultSet rs = null;
         ArrayList<Integer> movieIDs = new ArrayList<>();
@@ -150,103 +316,4 @@ public class WatchedDAO extends MysqlDAO implements WatchedDAOInterface
     }
 
 
-    @Override
-    public String AddtoWatched(String user, String movie, String title)
-    {
-        int userID = Integer.parseInt(user);
-        int movieID = Integer.parseInt(movie);
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        String result = "";
-        try
-        {
-            System.out.println("Fight Me");
-            con = this.getConnection();
-            String query = "INSERT INTO WATCHEDTABLE(USERID,MOVIEID,MOVIETITLE) VALUES(?,?,?)";
-            ps = con.prepareStatement(query);
-            ps.setInt(1, userID);
-            ps.setInt(2, movieID);
-            ps.setString(3, title);
-            ps.executeUpdate();
-            result = "SuccesfullyAdded";
-
-        }
-        catch (SQLException e)
-        {
-            System.out.println("SQL");
-            e.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
-                if (rs != null)
-                {
-                    rs.close();
-                }
-                if (ps != null)
-                {
-                    ps.close();
-                }
-                if (con != null)
-                {
-                    this.freeConnection(con);
-                }
-            }
-            catch (SQLException e)
-            {
-
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public Boolean CheckIfWatched(String user, String movie)
-    {
-        int userID = Integer.parseInt(user);
-        int movieID = Integer.parseInt(movie);
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try
-        {
-            con = this.getConnection();
-            String query = "SELECT * FROM WATCHEDTABLE WHERE USERID=? AND MOVIEID=?";
-            ps = con.prepareStatement(query);
-            ps.setInt(1, userID);
-            ps.setInt(2, movieID);
-            rs = ps.executeQuery();
-
-            return rs.next();
-        }
-        catch (SQLException e)
-        {
-
-        }
-        finally
-        {
-            try
-            {
-                if (rs != null)
-                {
-                    rs.close();
-                }
-                if (ps != null)
-                {
-                    ps.close();
-                }
-                if (con != null)
-                {
-                    this.freeConnection(con);
-                }
-            }
-            catch (SQLException e)
-            {
-            }
-        }
-
-        return false;
-    }
 }
--- src/main/java/DAOs/WatchedDAOInterface.java
+++ src/main/java/DAOs/WatchedDAOInterface.java
@@ -7,4 +7,5 @@ public interface WatchedDAOInterface
     public JSONArray GetAllWatchedMovies(String userID);
     public String AddtoWatched(String userID, String movieID, String title);
     public Boolean CheckIfWatched(String user,String movie);
+    public JSONArray GetAllWatchedMoviesForClient(String user);
 }
